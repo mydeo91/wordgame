359bd834ccf13a42bf5628511638c4edc66945a3
mydeo91 <leh2030@icloud.com>
2019-07-02 01:04:39 +0900

    Updates

---
+++ precache-manifest.35a08c0da7395c2e737f866236eded62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32ac4769a919197ec043331c7c7c249",
+    "url": "/wordgame/index.html"
+  },
+  {
+    "revision": "3c1eff610c44d85518ce",
+    "url": "/wordgame/static/css/main.bbd53a3d.chunk.css"
+  },
+  {
+    "revision": "7540c4b8a3285aabda4d",
+    "url": "/wordgame/static/js/2.e37e8b1b.chunk.js"
+  },
+  {
+    "revision": "3c1eff610c44d85518ce",
+    "url": "/wordgame/static/js/main.a0f62008.chunk.js"
+  },
+  {
+    "revision": "55ba5d4549745ce35d13",
+    "url": "/wordgame/static/js/runtime~main.0f5ed932.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wordgame/static/media/logo.5d5d9eef.svg"
+  }
+]);
